fcc868b18d92570de29fb2e44aee2373a76568a1
Grzegorz Holak <holi87@gmail.com>
2017-12-05 22:48:33 +0100

    mistype

--- datowe.py
+++ datowe.py
@@ -5,7 +5,7 @@ Tutaj będe wrzucał różne funkcje związane z datami
 '''
 from datetime import date
 
-dzien_tygodnia = date.today().strftime('%A')  # małe a zrobi skrócon dzień tygodnia
+dzien_tygodnia = date.today().strftime('%A')  # małe a zrobi skrócony dzień tygodnia
 data_dzisiejsza = date.today()
 
 print("Today is %s, %s" % (dzien_tygodnia, data_dzisiejsza))
