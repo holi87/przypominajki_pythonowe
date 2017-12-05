1a991445d63838132862f4bcbf10a5686b3edbe9
Grzegorz Holak <holi87@gmail.com>
2017-12-05 22:47:08 +0100

    dodatkowy komentarz nt skróconego dnia tygodnia

--- datowe.py
+++ datowe.py
@@ -5,7 +5,7 @@ Tutaj będe wrzucał różne funkcje związane z datami
 '''
 from datetime import date
 
-dzien_tygodnia = date.today().strftime('%A')
+dzien_tygodnia = date.today().strftime('%A')  # małe a zrobi skrócon dzień tygodnia
 data_dzisiejsza = date.today()
 
 print("Today is %s, %s" % (dzien_tygodnia, data_dzisiejsza))
