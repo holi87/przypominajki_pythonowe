f31a80ec072691446f971660a71b55a7ab0cb3d5
Grzegorz Holak <holi87@gmail.com>
2017-12-05 23:19:20 +0100

    printu printu

---
+++ printujemy.py
@@ -0,0 +1,8 @@
+print("a", "b", "c", "d")  # standard
+print("a", "b", "c", "d", sep=";")  # z separatorem (domyslnie spacja)
+print("a", "b", "c", "d", end=";")  # z innym końcem (domyslnie znak nowej linii)
+a = "a"
+b = "b"
+c = "c"
+d = "d"
+print("\n", "a", b, "#", c, "\t", "d")  # z nowa linia i tabulatorem ii hastagiem
