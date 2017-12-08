10d956f563e9ea94329748a6959b3f06c9f4f842
Grzegorz Holak <holi87@gmail.com>
2017-12-08 23:32:04 +0100

    wyszukiwanie napisu w cudzyslowiu

--- stringi.py
+++ stringi.py
@@ -19,6 +19,12 @@ print(jakis_tekst.lower())  # jw tylko na male
 print(jakis_tekst.islower())  # sprawdza czy string jest stworzony z malych liter, isupper odpowiednio czy z duzych
 print(jakis_tekst.find("ba", 6))  # zwraca na kotrym miejscu jest w stringu podany string w argumencie
                                   # oraz od ktorego zaczynamy (mozna tez podac na ktorym konczymy szukanie)
+# sztuczka z wyszukiwaniem, np czegos w cudzyslowiu:
+tekst = 'sialala "ahahaha" dadada'
+temp = tekst[tekst.find('"')+1:]
+print(temp[:temp.find('"')])
+# koniec sztuczki
+
 print(jakis_tekst.replace('ba', 'ka'))  # zamienia string(znak na nowy string (znak)
 print(jakis_tekst.split("a"))  # zwraca liste, a jako parametr podajemy separator ktory oddziela elementy listy
 print("".join(reversed(jakis_tekst)))  # a to odwraca tekst
